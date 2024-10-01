e96c925733004478726a4f58d6c8b3b4aebd17ab
chaebinkim <chaebin405@gmail.com>
2024-09-30 20:37:25 -0400

    Update Fit.jl

--- src/Fit.jl
+++ src/Fit.jl
@@ -30,6 +30,7 @@ function Fit(Objective, interval, max_iter; file_name = "Bopt_Log", fig_name = "
         print("Initial Run")
         idx_list = np.array([start])
         X = np.vstack([np.random.uniform(low=bounds[p][0], high=bounds[p][1], size=50-len(bounds)) for p in bounds]).T
+        X = X.round(decimals = 5, out = None)
         params ={"ID":1}
         for p,i in zip(bounds, range(len(bounds))):
             params[p] = X[0,i]
