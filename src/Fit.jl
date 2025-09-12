a54fc32af588f59e5042eb6078e109a1105ead3d
Chaebin Kim <chaebin405@gmail.com>
2025-09-12 01:11:51 -0400

    Use delta variable instead of hardcoded value

--- src/Fit.jl
+++ src/Fit.jl
@@ -283,7 +283,7 @@ chi2_min = float(np.min(chi2s))
 out = levelset_region_sampling(
     GP, bounds, param_order,
     chi2_min=chi2_min,   # 생략해도 chi2s.min() 사용
-    delta=1.0,               # 원하는 값으로
+    delta=delta,               # 원하는 값으로
     n_samples=500,
     X_hist=X, chi2_hist=chi2s,
     q=None,                  # 저장값을 기대값 기준으로. 보수적이면 0.95 등
